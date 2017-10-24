77be11ea8452b34e04e879b89a9b0bd038facda0
Daniel Medrano <judamedranoba@ittepic.edu.mx>
2017-10-24 12:02:07 -0600

    Modifications

--- app/src/main/java/mx/edu/ittepic/judamedranoba/recordatec/login.java
+++ app/src/main/java/mx/edu/ittepic/judamedranoba/recordatec/login.java
@@ -3,6 +3,7 @@ package mx.edu.ittepic.judamedranoba.recordatec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class login extends AppCompatActivity {
 
     @Override
